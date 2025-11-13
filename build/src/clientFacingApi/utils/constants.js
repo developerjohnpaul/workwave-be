d3cf239efa0ffa387fe2348d851e59f3a5a33a5a
Johnpaul <developerjohnpaul@gmail.com>
2025-11-13 19:30:09 +0100

    fixes

--- build/src/clientFacingApi/utils/constants.js
+++ build/src/clientFacingApi/utils/constants.js
@@ -11,7 +11,7 @@ exports.mysql = require("mysql");
 }
 exports.transporter = nodemailer_1.default.createTransport({
     host: process?.env?.SMTP_HOST,
-    port: Number(process?.env?.SMTP_PORT),
+    port: 587,
     secure: false,
     auth: {
         user: process?.env?.SMTP_USER,
--- src/clientFacingApi/utils/constants.ts
+++ src/clientFacingApi/utils/constants.ts
@@ -5,7 +5,7 @@ export const mysql = require("mysql");
 }
 export const transporter = nodemailer.createTransport({
     host: process?.env?.SMTP_HOST,
-    port:  Number(process?.env?.SMTP_PORT),
+    port:  587,
     secure:false,
     auth: {
     user: process?.env?.SMTP_USER,
